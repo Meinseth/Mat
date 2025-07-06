671748ffe84a307833c5e71dd414cfebd39a2c74
Meinseth <vebjorn@meinseth.no>
2025-07-07 01:57:55 +0200

    docker compose

--- Mat.Api/Database/DbContext.cs
+++ Mat.Api/Database/DbContext.cs
@@ -5,9 +5,4 @@ class MatDbContext(DbContextOptions<MatDbContext> options) : DbContext(options)
 {
     public DbSet<Recipe> Recipes => Set<Recipe>();
     public DbSet<Ingredient> Ingredients => Set<Ingredient>();
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
-        optionsBuilder.UseSqlServer(
-            "Server=localhost;Database=Mat;Trusted_Connection=True;TrustServerCertificate=True;"
-        );
 }
--- Mat.Api/Program.cs
+++ Mat.Api/Program.cs
@@ -2,7 +2,9 @@ using Mat.Endpoints;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<MatDbContext>();
+builder.Services.AddDbContext<MatDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApiDocument(config =>
@@ -11,6 +13,7 @@ builder.Services.AddOpenApiDocument(config =>
     config.Title = "MatAPI v1";
     config.Version = "v1";
 });
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -38,6 +41,12 @@ else
 {
     // Optional: Add a different root route in production
     app.MapGet("/", () => "API is running.");
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<MatDbContext>();
+    db.Database.Migrate(); // Applies migrations automatically
 }
 
 app.AddRecipesEndpoints();
