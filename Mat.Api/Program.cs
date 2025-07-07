ccf2242c620d8f8697f16fc70f85ba6f5e7b0901
Meinseth <vebjorn@meinseth.no>
2025-07-07 02:29:04 +0200

    show swagger

--- Mat.Api/Program.cs
+++ Mat.Api/Program.cs
@@ -16,32 +16,14 @@ builder.Services.AddOpenApiDocument(config =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseOpenApi();
+app.UseSwaggerUi(config =>
 {
-    app.UseOpenApi();
-    app.UseSwaggerUi(config =>
-    {
-        config.DocumentTitle = "MatAPI";
-        config.Path = "/swagger";
-        config.DocumentPath = "/swagger/{documentName}/swagger.json";
-        config.DocExpansion = "list";
-    });
-
-    // Redirect "/" to Swagger UI in development
-    app.MapGet(
-        "/",
-        context =>
-        {
-            context.Response.Redirect("/swagger");
-            return Task.CompletedTask;
-        }
-    );
-}
-else
-{
-    // Optional: Add a different root route in production
-    app.MapGet("/", () => "API is running.");
-}
+    config.DocumentTitle = "MatAPI";
+    config.Path = "/swagger";
+    config.DocumentPath = "/swagger/{documentName}/swagger.json";
+    config.DocExpansion = "list";
+});
 
 using (var scope = app.Services.CreateScope())
 {
