59133db0b76abebd067f9a4f29f3008c3abe916a
Vebjørn Meinseth <vebjorn@meinseth.no>
2025-07-21 01:32:21 +0200

    display recipes list

--- Mat.Frontend/src/App.tsx
+++ Mat.Frontend/src/App.tsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/recipes")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => console.error("Fetch error:", err));
+  }, []);
+
   return (
     <>
       <div></div>
       <h1>Mat</h1>
+      <h1>{message}</h1>;
     </>
   );
 }
