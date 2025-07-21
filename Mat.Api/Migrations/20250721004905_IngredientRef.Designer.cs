08aad2b85644e26de49595322956124f83fc02b4
Vebjørn Meinseth <vebjorn@meinseth.no>
2025-07-21 02:51:12 +0200

    migration ingredientRef

--- Mat.Api/Migrations/20250721004905_IngredientRef.Designer.cs
+++ Mat.Api/Migrations/20250721004905_IngredientRef.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Mat.Migrations
 {
     [DbContext(typeof(MatDbContext))]
-    partial class MatDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250721004905_IngredientRef")]
+    partial class IngredientRef
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
