449b22383f516d9c0af4f2d54470e3075247c34b
Meinseth <vebjorn@meinseth.no>
2025-08-16 19:34:58 +0200

    update setup.sql

--- Mat.Db/setup.sql
+++ Mat.Db/setup.sql
@@ -1,22 +1,19 @@
--- Create database if it doesn't exist
+USE master;
+
+-- Create database
 IF DB_ID('Mat') IS NULL
     CREATE DATABASE Mat;
 GO
 
--- Create login if not exists
+-- Create login
 IF NOT EXISTS (SELECT * FROM sys.sql_logins WHERE name = 'matuser')
-BEGIN
-    DECLARE @sql NVARCHAR(MAX) = 'CREATE LOGIN matuser WITH PASSWORD = $(DB_PASSWORD);';
-    EXEC sp_executesql @sql;
-END
+    CREATE LOGIN matuser WITH PASSWORD = N'${DB_PASSWORD}';
 GO
 
--- Map login to database user
+-- Create user in the database
 USE Mat;
 IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'matuser')
-BEGIN
     CREATE USER matuser FOR LOGIN matuser;
-    ALTER ROLE db_datareader ADD MEMBER matuser;
-    ALTER ROLE db_datawriter ADD MEMBER matuser;
-END
+ALTER ROLE db_datareader ADD MEMBER matuser;
+ALTER ROLE db_datawriter ADD MEMBER matuser;
 GO
