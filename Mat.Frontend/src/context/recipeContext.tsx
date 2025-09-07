561087ac8555e0438ea29cf4a24cf5b97cc61b81
Meinseth <vebjorn@meinseth.no>
2025-09-07 20:36:14 +0200

    fix docker

--- Mat.Frontend/src/context/recipeContext.tsx
+++ Mat.Frontend/src/context/recipeContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import { ApiClient, type RecipeDto } from "../services/ApiClient";
 import { ApiBaseUrl } from "../services/ApiBaseUrl";
 
