d1b93fc0855a9eb4686b807991115fa075dd325f
Meinseth <vebjorn@meinseth.no>
2025-09-27 14:38:45 +0200

    UseHttpsRedirection

--- Mat.Api/Program.cs
+++ Mat.Api/Program.cs
@@ -128,6 +128,7 @@ app.UseForwardedHeaders();
 if (builder.Environment.IsDevelopment())
     app.UseCors("AllowLocal");
 
+app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.AddRecipesEndpoints();
